2b2f6b648fbf0d8a4998efbd3301e580e73342c3
Oleg <olegpsiarovsky@gmail.com>
2018-11-05 22:27:32 +0200

    Update project

--- BirthdayTracker/BirthdayTracker/AddBirthdayViewController.swift
+++ BirthdayTracker/BirthdayTracker/AddBirthdayViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import CoreData
-
+import UserNotifications
 
 class AddBirthdayViewController: UIViewController {
 
@@ -39,6 +39,19 @@ class AddBirthdayViewController: UIViewController {
         
         do {
             try context.save()
+            let message = "Today is \(firstName) \(lastName) birthday"
+            let content = UNMutableNotificationContent()
+            content.body = message
+            content.sound = UNNotificationSound.default
+            var dateComponents = Calendar.current.dateComponents([.month, .day], from: birthdate)
+            dateComponents.hour = 20
+            dateComponents.minute = 40
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+            if let identifier = newBirthday.birthdayId {
+                let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+                let center = UNUserNotificationCenter.current()
+                center.add(request, withCompletionHandler: nil)
+            }
         } catch let error {
             print("SAVE ERROR!!! -- \(error)")
         }
